6465fb2bce3f5bef618fb8d6117d4fca5fa43d42
PSheon <pauljiang61020@gmail.com>
2024-03-01 13:50:46 +0800

    feat: improved seo tags.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,6 +12,34 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Meta */}
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* Link */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/seo/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/seo/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/seo/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/seo/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/seo/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Bakbak+One&family=Inter:wght@300;400&display=swap"
           rel="stylesheet"
